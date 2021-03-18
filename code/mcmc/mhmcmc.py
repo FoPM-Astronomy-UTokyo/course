00e35789c05e74802b2f67976c5f73af5236f5fc
Ryou Ohsawa <xr0038@gmail.com>
2021-03-18 21:35:11 +0900

    update mhmcmc.py
    drop AbstractLikelihood

--- code/mcmc/mhmcmc.py
+++ code/mcmc/mhmcmc.py
@@ -1,28 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from typing import Union, Optional, Callable
 from numpy.random import default_rng
+from tqdm import tqdm, trange
 import numpy as np
 
 
-class AbstractLikelihood:
-  ''' A template of likelihood function.'''
+class AbstractProposalDistribution(object):
+  ''' A template of proposal distribution class. '''
 
-  def eval(self, x: np.ndarray):
-    ''' Evaluate the log-likelihood for the state `x`.
-
-    Parameters:
-      x (numpy.ndarray): An input state (1-dimensional vector).
-
-    Returns:
-      float: The log-likelihood, logL(x).
-    '''
-    pass
-
-
-class AbstractProposalDistribution:
-  ''' A template of proposed distribution. '''
-
-  def draw(self, x0: np.ndarray):
+  def draw(self, x0: np.ndarray) -> np.ndarray:
     ''' Propose a new state.
 
     Parameters:
@@ -31,39 +18,119 @@ class AbstractProposalDistribution:
     Returns:
       numpy.ndarray: A newly-proposed state.
     '''
+    pass
 
-  def eval(self, x: np.ndarray, x0: np.ndarray):
-    ''' Evaluate the log-probability for the state `x`.
+  def eval(self, x: np.ndarray, x0: np.ndarray) -> float:
+    ''' Evaluate the log-transition probability for the state `x`.
 
     Parameters:
       x  (numpy.ndarray): The proposed state.
       x0 (numpy.ndarray): The current state.
 
     Returns:
-      float: The log-probability, logQ(x, x0).
+      float: The log-transition probability, logQ(x, x0).
     '''
+    pass
 
 
-class MHMCMCSampler:
+class GaussianStep(AbstractProposalDistribution):
+  ''' A random-walk proposal distribution with Gaussian distribution. '''
+
+  def __init__(
+      self, sigma: Union[float, np.ndarray], seed: int = 2021) -> None:
+    ''' Generate an instance.
+
+    Args:
+      sigma (float or numpy.ndarray): Length of a Monte Carlo step.
+      seed (int, optional): Seed value for the random value generator.
+    '''
+    self.sigma = sigma
+    self.gen = default_rng(seed)
+
+  def draw(self, x0: np.ndarray) -> np.ndarray:
+    ''' Propose a new state by random walk.
+
+    Parameters:
+      x0 (numpy.ndarray): The current state.
+
+    Returns:
+      numpy.ndarray: A newly-proposed state.
+    '''
+    return x0 + self.gen.normal(0, self.sigma, size=x0.shape)
+
+  def eval(self, x: np.ndarray, x0: np.ndarray) -> float:
+    ''' Evaluate the log-transition probability for the state `x`.
+
+    Parameters:
+      x  (numpy.ndarray): The proposed state.
+      x0 (numpy.ndarray): The current state.
+
+    Returns:
+      float: The log-transition probability, logQ(x, x0).
+    '''
+    return np.sum(-(x-x0)**2/(2*self.sigma**2))
+
+
+class CauchyStep(AbstractProposalDistribution):
+  ''' A random-walk proposal distribution with Cauchy distribution. '''
+
+  def __init__(
+      self, scale: Union[float, np.ndarray], seed: int = 2021) -> None:
+    ''' Generate an instance.
+
+    Args:
+      scale (float or numpy.ndarray): Length of a Monte Carlo step.
+      seed (int, optional): Seed value for the random value generator.
+    '''
+    self.scale = scale
+    self.gen = default_rng(seed)
+
+  def draw(self, x0: np.ndarray) -> np.ndarray:
+    ''' Propose a new state by random walk.
+
+    Parameters:
+      x0 (numpy.ndarray): The current state.
+
+    Returns:
+      numpy.ndarray: A newly-proposed state.
+    '''
+    return x0 + self.scale * self.gen.standard_cauchy(size=x0.shape)
+
+  def eval(self, x: np.ndarray, x0: np.ndarray) -> float:
+    ''' Evaluate the log-transition probability for the state `x`.
+
+    Parameters:
+      x  (numpy.ndarray): The proposed state.
+      x0 (numpy.ndarray): The current state.
+
+    Returns:
+      float: The log-transition probability, logQ(x, x0).
+    '''
+    return -np.sum(np.log(1.0 + ((x-x0)/self.scale)**2))
+
+
+class MHMCMCSampler(object):
   ''' MCMC sampler with the Metropolis-Hastings algorithm. '''
 
-  def __init__(self, likelihood: AbstractLikelihood,
-               proposal_dist: AbstractProposalDistribution, seed: int=2021):
+  def __init__(self,
+      log_likelihood: Callable[[np.ndarray], float],
+      proposal_dist: AbstractProposalDistribution, seed: int=2021) -> None:
     ''' Generate a MCMC sampler instatnce.
 
     Parameters:
-      likelihood (obj): An instance to calculate log-likelihood.
+      likelihood (function): An instance to calculate log-likelihood.
         A sub-class of AbstractLikelihood is preferred.
-      proposal_dist (obj): An instance to draw from a proposal distribution.
+      proposal_dist (AbstractProposalDistribution):
+        An instance to draw from a proposal distribution.
         A sub-class of AbstractProposalDistribution is preferred.
       seed (float, optional): Random seed value.
     '''
-    self.likelihood = likelihood
+    self.log_likelihood = log_likelihood
     self.proposal_dist = proposal_dist
     self.random = default_rng(seed)
     self.initialize(None)
 
-  def initialize(self, x0: np.ndarray):
+  def initialize(self, x0: np.ndarray) -> None:
     ''' Initialize state.
 
     Parameters:
@@ -71,7 +138,7 @@ class MHMCMCSampler:
     '''
     self.state = x0
 
-  def step(self):
+  def step_forward(self) -> None:
     ''' Draw a next Monte-Carlo step.
 
     Returns:
@@ -79,13 +146,13 @@ class MHMCMCSampler:
     '''
     proposal = self.proposal_dist.draw(self.state)
     log_alpha = \
-      self.likelihood.eval(proposal) - self.likelihood.eval(self.state) \
+      self.log_likelihood(proposal) - self.log_likelihood(self.state) \
       + self.proposal_dist.eval(self.state, proposal) \
       - self.proposal_dist.eval(proposal, self.state)
     log_u = np.log(self.random.uniform(0, 1))
-    return proposal if (log_u < log_alpha) else self.state
+    self.state = proposal if (log_u < log_alpha) else self.state
 
-  def generate(self, n_sample: int):
+  def generate(self, n_sample: int) -> np.ndarray:
     ''' Generate N-samples
 
     Parameters:
@@ -94,20 +161,26 @@ class MHMCMCSampler:
     Returns:
       numpyn.ndarray: A table of generated MCMC samples.
     '''
+    if self.state is None:
+      raise RuntimeError('state is not initialized.')
     samples = []
-    for n in range(n_sample):
+    tqdmfmt = '{l_bar}{bar}| {n_fmt}/{total_fmt}'
+    for n in trange(n_sample, bar_format=tqdmfmt):
+      self.step_forward()
       samples.append(self.state)
-      self.state = self.step()
     return np.vstack(samples)
 
 
-def display_results(res: np.ndarray, dim: object=None, skip: int=1,
-                    output: str=None):
+def display_results(
+    res: np.ndarray,
+    dim: Union[int, np.ndarray, None]=None,
+    skip: int=1, output: str=None) -> None:
   ''' Display the trace and histogram of samples.
 
   Parameters:
     res (numpy.ndarray): MCMC sample with the size of [Nample, Ndim].
-    dim (int, optional): Dimension to be investigated. If not specified,
+    dim (int or numpy.ndarray, optional):
+        Dimension to be investigated. If not specified,
         The first dimension is selected by default.
     skip (int, optional): Pick up every {skip} sample. If not specified,
         Do not skip any samples by default.
@@ -126,7 +199,7 @@ def display_results(res: np.ndarray, dim: object=None, skip: int=1,
     val = res[::skip,d]
     ax[0].plot(val)
     ax[0].set_ylabel(f'trace of X{d}')
-    ax[1].hist(val, bins=min(200,max(int(val.size/25),20)), density=True)
+    ax[1].hist(val, bins=min(100,max(int(val.size/25),20)), density=True)
     ax[1].set_ylabel(f'frequency of X{d}')
   plt.tight_layout()
   if output: fig.savefig(output)
@@ -138,42 +211,41 @@ if __name__ == '__main__':
   from argparse import ArgumentParser as ap
   parser = ap(description='demo for HMMCMCSampler')
   parser.add_argument(
-    '-n', '--num', dest='num', metavar='n', type=int, default=10000,
+    '-n', '--num', dest='num', metavar='n', type=int, default=3000,
     help='number of monte carlo samples.')
   parser.add_argument(
     '-b', '--burnin', dest='burn', metavar='n', type=int, default=1000,
     help='number of burn-in steps.')
   parser.add_argument(
-    '--step', metavar='n', type=float, default=0.2,
+    '--step', metavar='n', type=float, default=0.5,
     help='step width of a Monte Carlo step.')
   parser.add_argument(
     '--png', metavar='filename', type=str, help='output figure name.')
 
   args = parser.parse_args()
 
-  class logL(AbstractLikelihood):
-    def __init__(self, center=[3, 5, -1], sigma=1.0):
-      self.center = np.array(center)
-      self.sigma = sigma
-    def eval(self, x):
-      return np.sum(-(x-self.center)**2/2.0/self.sigma**2)
+  def logL(x: np.array) -> float:
+    center = np.array([3.2, 5.9, 1.1, 8.8])
+    sigma  = np.array([0.8, 0.5, 0.4, 1.2])
+    return np.sum([-((z-c)/s)**2/2 for z,c,s in zip(x,center,sigma)])
 
-  class logQ(AbstractProposalDistribution):
-    def __init__(self, step):
-      self.step = step
-      self.gen = default_rng(2021)
-    def draw(self, x0):
-      return x0 + self.gen.normal(0, self.step, size=x0.shape)
-    def eval(self, x, x0):
-      return np.sum(-(x-x0)**2/(2*self.step**2))
+  step = GaussianStep(args.step)
+  mcmc = MHMCMCSampler(logL, step)
 
-  mcmc = MHMCMCSampler(logL(), logQ(args.step))
-
-  x0 = np.zeros(3)
+  x0 = np.zeros(4)
   mcmc.initialize(x0)
 
 
   val = mcmc.generate(args.num + args.burn)
   val = val[args.burn:]
 
-  display_results(val, skip=5, output=args.png)
+  mean = val.mean(axis=0)
+  std  = val.std(axis=0)
+  print('mean value')
+  print('  ground truth   : [3.20 5.90 1.10 8.80]')
+  print('  estimated value: [{:.2f} {:.2f} {:.2f} {:.2f}]'.format(*mean))
+  print('stddev value')
+  print('  ground truth   : [0.80 0.50 0.40 1.20]')
+  print('  estimated value: [{:.2f} {:.2f} {:.2f} {:.2f}]'.format(*std))
+
+  display_results(val, skip=30, output=args.png)
